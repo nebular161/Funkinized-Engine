ed4e1619c69cba6d2eaad263c554e0056a38b360
FizzyBot <mranimationmaker27flip@gmail.com>
2023-01-15 13:29:03 -0500

    new health bar

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -826,6 +826,7 @@ class PlayState extends MusicBeatState {
 		healthBar = new FlxBar(healthBarBG.x + 4, healthBarBG.y + 4, RIGHT_TO_LEFT, Std.int(healthBarBG.width - 8), Std.int(healthBarBG.height - 8), this,
 			'health', 0, 2);
 		healthBar.scrollFactor.set();
+		healthBar.width + 350;
 		healthBar.createFilledBar(0xFFFF0000, 0xFF66FF33);
 		add(healthBar);
 
