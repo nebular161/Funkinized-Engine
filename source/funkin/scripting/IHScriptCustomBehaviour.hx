947c30fa0a4d9a95db6a1dbb9af46750075b8754
FizzyBot <mranimationmaker27flip@gmail.com>
2023-01-30 18:49:41 -0500

    used the wrong hscript files woops

--- source/funkin/scripting/IHScriptCustomBehaviour.hx
+++
@@ -1,6 +0,0 @@
-package funkin.scripting;
-
-interface IHScriptCustomBehaviour {
-    public function hset(name:String, val:Dynamic):Dynamic;
-    public function hget(name:String):Dynamic;
-}
---
+++ source/funkin/scripting/PlayStateWorkspace.hx
@@ -0,0 +1,8 @@
+package funkin.scripting;
+
+interface PlayStateWorkspace 
+{
+    public function create():Void;
+    
+    public function update(elapsed:Float):Void;    
+}
