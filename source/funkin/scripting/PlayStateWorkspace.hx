cf67ebeca4fafd94f3da1acf45c9b05be4eab8d9
FizzyBot <mranimationmaker27flip@gmail.com>
2023-01-30 19:06:22 -0500

    meh

--- source/funkin/scripting/PlayStateWorkspace.hx
+++ source/funkin/scripting/PlayStateWorkspace.hx
@@ -4,5 +4,9 @@ interface PlayStateWorkspace
 {
     public function create():Void;
     
-    public function update(elapsed:Float):Void;    
+    public function update(elapsed:Float):Void;
+
+    public function beatHit(curBeat:Int):Void;
+
+    public function stepHit(curBeat:Int):Void;
 }
